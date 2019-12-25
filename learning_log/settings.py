b1af651d530c3bdd7591158170f02ff39c8ba70e
lenivaya <xocada@gmail.com>
2019-12-25 22:16:50 +0200

    typo again

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -133,7 +133,7 @@ BOOTSTRAP3 = {
 # Heroku settings
 cwd = os.getcwd()
 if cwd == '/app' or cwd[:4] == '/tmp':
-    import dj_databse_url
+    import dj_database_url
     DATABASES = {
         'default': dj_database_url.config(default='postgres://localhost')
     }
